7e35be64cd02b536e7ff2954e25afbf89fc05256
MohammadReza Arasteh <m.arasteh@dotin.ir>
2023-09-27 16:27:01 +0330

    Add middlewares to /chats

--- src/routes/chats.ts
+++ src/routes/chats.ts
@@ -1,25 +1,26 @@
 import express from 'express';
 import * as chatController from '../controllers/chatController';
 import multer from "../utils/multer";
+import auth from "../middlewares/auth";
 
 const router = express.Router();
 
-router.post("/", multer.single("chatImage"), chatController.postChat);
+router.post("/", [auth, multer.single("chatImage")], chatController.postChat);
 
-router.post("/:id/members/", chatController.postMember);
+router.post("/:id/members/", auth, chatController.postMember);
 
-router.put("/:id/members", chatController.removeMember);
+router.put("/:id/members", auth, chatController.removeMember);
 
-router.post("/:id/message", chatController.postMessage);
+router.post("/:id/message", auth, chatController.postMessage);
 
-router.put("/:id/message", chatController.editMessage);
+router.put("/:id/message", auth, chatController.editMessage);
 
-router.delete("/:id", chatController.deleteChat);
+router.delete("/:id", auth, chatController.deleteChat);
 
-router.delete("/:id/message/:messageID", chatController.deleteMessage);
+router.delete("/:id/message/:messageID", auth, chatController.deleteMessage);
 
-router.get("/:id", chatController.getChat);
+router.get("/:id", auth, chatController.getChat);
 
-router.get("/:id/private/:targetId", chatController.checkPrivate);
+router.get("/:id/private/:targetId", auth, chatController.checkPrivate);
 
 export default router;
--- src/startup/routes.ts
+++ src/startup/routes.ts
@@ -1,13 +1,11 @@
 import express, {Application} from "express";
 import user from "../routes/users";
 import chat from "../routes/chats";
-import auth from "../routes/auth";
 import test from "../routes/test";
 
 export default function (app: Application) {
     app.use("/uploads", express.static("uploads"));
     app.use("/api/v1/users", user);
     app.use("/api/v1/chats", chat);
-    app.use("/api/v1/auth", auth);
     app.use("/api/v1/test", test);
 };
